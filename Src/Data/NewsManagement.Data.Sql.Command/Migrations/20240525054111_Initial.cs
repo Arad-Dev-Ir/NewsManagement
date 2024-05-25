41bf0377280e49d565f6fb0365a92b87aea4b59a
Farshid Kazemi <farshidkazemi.dev@outlook.com>
2024-05-25 09:14:37 +0330

    feature: Apply database's initial migration
    This commit applies EF Core initial migration for application, which allows to save and retrieve data.
    
    The feature includes initial migraion file and Snapshot for EF Core in Data.Sql.Command layer.

---
+++ Src/Data/NewsManagement.Data.Sql.Command/Migrations/20240525054111_Initial.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewsManagement.Data.Sql.Command.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Type = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Data = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false),
+                    UserId = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Mode = table.Column<string>(type: "nvarchar(20)", maxLength: 20, nullable: false),
+                    ModuleId = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    ModuleName = table.Column<string>(type: "varchar(255)", unicode: false, maxLength: 255, nullable: false),
+                    ModuleType = table.Column<string>(type: "varchar(500)", unicode: false, maxLength: 500, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "News",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    Body = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Code = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_News", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NewsKeywords",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KeywordCode = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedByUserId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    CreatedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedByUserId = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    ModifiedDateTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NewsId = table.Column<long>(type: "bigint", nullable: true),
+                    Code = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NewsKeywords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_NewsKeywords_News_NewsId",
+                        column: x => x.NewsId,
+                        principalTable: "News",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NewsKeywords_NewsId",
+                table: "NewsKeywords",
+                column: "NewsId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Events");
+
+            migrationBuilder.DropTable(
+                name: "NewsKeywords");
+
+            migrationBuilder.DropTable(
+                name: "News");
+        }
+    }
+}
